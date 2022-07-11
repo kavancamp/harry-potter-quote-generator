af0cd4199cfc37724a567b3bf3178dc0db03c289
Keenah <kvancamp@mail.com>
2022-07-11 10:57:03 -0500

    added speechsynthesis

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const quoteText = document.querySelector(".quote"),
+const quote = document.querySelector(".quote"),
 author = document.querySelector(".author .name"),
 btn = document.querySelector("button"),
 copyBtn = document.querySelector(".copy"),
 snapBtn = document.querySelector(".snapchat"),
-soundBtn = document.querySelector(".sound");
+playBtn = document.querySelector(".sound");
 
 genRandomQuote = () => {
     fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
         console.log(result);
-        quoteText.innerText = result.content;
-        authorName.innerText = result.content;
+        quote.innerText = result.content;
+        author.innerText = result.author;
     });
 }
+
+playBtn.addEventListener("click", () => {
+    let vocate = new SpeechSynthesisUtterance(`${quote.innerText} by ${author.innerText}`); //text to speech api
+    speechSynthesis.speak(vocate); 
+    //speak method that 'plays' the text
+});
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(quote.innerText);
+    //copies on click - writeText writes to clipboard
+});
 
 btn.addEventListener("click", genRandomQuote);
