e9aed583ef7223f54a1a9ea8cf4d5340f5b9987e
Keenah <kvancamp@mail.com>
2022-07-11 09:01:19 -0500

    added btn querySelectors

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const quoteText = document.querySelector(".quote"),
-btn = document.querySelector("button");
-
+author = document.querySelector(".author .name"),
+btn = document.querySelector("button"),
+copyBtn = document.querySelector(".copy"),
+snapBtn = document.querySelector(".snapchat"),
+soundBtn = document.querySelector(".sound");
 
 genRandomQuote = () => {
     fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
         console.log(result);
         quoteText.innerText = result.content;
+        authorName.innerText = result.content;
     });
 }
 
